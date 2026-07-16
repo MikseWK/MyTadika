436220d550601d590ae5e099708b3cac9c0cd28b
MikseWK <liewwaikit5pgta5@gmail.com>
2026-07-16 20:38:07 +0800

    delete unecessary files

--- backend/src/main/java/com/mytadika/model/MemoryReaction.java
+++ backend/src/main/java/com/mytadika/model/MemoryReaction.java
@@ -1,7 +1,18 @@
 package com.mytadika.model;
-import jakarta.persistence.*;
-import lombok.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity @Table(name = "memory_reactions")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
